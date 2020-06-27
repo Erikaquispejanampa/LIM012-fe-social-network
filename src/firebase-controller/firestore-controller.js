7dca0672ea642a3df30c639781654698f03c928a
jael-VRR <jrodriguezruidias@gmail.com>
2020-06-26 20:42:38 -0500

    firebase

--- src/firebase-controller/firestore-controller.js
+++ src/firebase-controller/firestore-controller.js
@@ -1,29 +1,19 @@
 
 
-/* export const createData = (table ,id , json) => {
-    console.log('table',table);
-    console.log('id',id);
-    console.log('json',json);
-    firebase.firestore().collection(table).doc(id).set(json);
+export const createData = (table, id, json) => {
+  console.log('table', table);
+  console.log('id', id);
+  console.log('json', json);
+  firebase.firestore().collection(table).doc(id).set(json);
 };
 
 export const getData = (table, id) => {
-    const docRef = firebase.firestore().collection(table).doc(id);
-    return docRef.get();
-}; */
-
-export const createProfileInfo = (obj) => {
-  firebase.firestore().collection('users').doc(obj.uid).set({
-    displayName: obj.displayName,
-    fotoUser: obj.photoURL,
-    emailUser: obj.email,
-  });
+  const docRef = firebase.firestore().collection(table).doc(id);
+  return docRef.get();
 };
 
-
+/* --creando documento---*/
 export const getUser = (docId) => {
   const docRef = firebase.firestore().collection('users').doc(docId);
   return docRef.get();
 };
-
-export const getProfileInfo = userId => firebase.firestore().collection('users').doc(userId).get();

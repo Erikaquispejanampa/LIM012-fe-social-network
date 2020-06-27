aed9d7fdff1b3bc98e5f5db8bf5847a5e74a2e08
jael-VRR <jrodriguezruidias@gmail.com>
2020-06-27 08:28:29 -0500

    ruta

--- src/view/index.js
+++ src/view/index.js
@@ -3,12 +3,14 @@ import Login from './login.js';
 import Register from './register.js';
 import Home from './home.js';
 import NotFound from './notFound.js';
+import Profile from './profile.js';
 
 const components = {
   login: Login,
   register: Register,
   error: NotFound,
   home: Home,
+  profile: Profile,
 };
 
 export { components };

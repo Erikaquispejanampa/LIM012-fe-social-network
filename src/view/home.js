559d86d35b4f0d432868df2b5dee77f26ced3831
jael-VRR <jrodriguezruidias@gmail.com>
2020-06-27 08:28:01 -0500

    modificacion vista home

--- src/view/home.js
+++ src/view/home.js
@@ -18,7 +18,7 @@ export default () => {
   <header class="header">
        <nav class="nav-home">
       <ul>
-        <li><a href="#"><span class ="icon-user"></span>Perfil</a></li>
+        <li><a href="#/profile"><span class ="icon-user"></span>Perfil</a></li>
         <li><a id="btnSignOut" href="#/login"><span class ="icon-switch"></span>cerrar sesión</a></li>
         
       </ul>
@@ -28,11 +28,11 @@ export default () => {
   <main class="contenido">
   <div class="profile-section">
   <div class="cover-image"></div>
-  <div class="profile">
+  <div class="sectionDataUser">
     <div class="profile-photo-name">
-      <div class="profile-picture">
-        <img  class="profile-picture" src="${dataUser.photoURL || './img/profile-ico.png'}" alt="">
-      </div>
+      <section class="box-picture">
+        <img  class="box-picture" src="${dataUser.photoURL || './img/profile-ico.png'}">
+      </section>
       <h2 class="user-name">${dataUser.displayName}</h2>
     </div>
     </div>
@@ -40,21 +40,17 @@ export default () => {
   </main>
   <aside class="sidebar">
   <section class="container-home">
-  
-  
-        
             <div class="content-publications">
-              <textarea class="new-post" id="newPost" rows="6" cols="70" placeholder="¿Qué quisieras compartir?"></textarea>
-              <img src="img/foto.png" class="upImage"  style="width:30px; height:30px;">
-              <select name="selectPublicPrivate" class="selectPublicPrivate" id="optionPublicPrivate">
-              <option value="publico" class="fas fa-users"> 🌎 <span>Público</span></option>
-              <option value="privado" class="fas fa-user-lock"> 🔐 <span>Privado </span></option>
-            </select>
-            <div class="buttons-new-post">
+               <textarea class="new-post" id="newPost" rows="6" cols="70" placeholder="¿Qué quisieras compartir?"></textarea>
+                <img src="img/foto.png" class="upImage"  style="width:30px; height:30px;">
+                <img src="img/seguridad.png"  id="privacity" style="width:30px; height:30px;">
+             
+       
+             <label class="btnSend">
                 <button id="btnNewPost"  class="btn-post">Publicar</button>
-              </div>
+                <img src="img/avion.png"  id="send" style="width:30px; height:30px;">
+             </label>
             </div>
-            <br>
             <br>
           
             <div class="templateComments">
@@ -65,17 +61,60 @@ export default () => {
               <div id="updatePost">
                
                <div class="textComment">Comenta aquí</div>
+             
               </div>
               <div class="wall">
               
               <label class= "btnFile">
-               <input type="file" id="image"  accept="image/jpeg, image/png, image/gif" class="upload">
+               <input type="file" id="selectImage"  accept="image/jpeg, image/png, image/gif" class="upload">
                <img src="img/foto.png" class="upImage"  style="width:30px; height:30px;">
               </label> 
               <img src="img/like.png"  id="like" style="width:30px; height:30px;">
-              <img src="img/seguridad.png"  id="privacity" style="width:30px; height:30px;">
+              <img src="img/comentario.png"  id="comment" style="width:30px; height:30px;">
               </div>
             </div>
+            <br>
+            <div class="templateComments">
+             <div class ="user-wall">
+              <img  class="user-foto" src="${dataUser.photoURL}"  style="width:50px; height:50px; border-radius:50%;  margin:auto; left:0; top:0; rigth:0; bottom:0;">
+             </div>
+            
+              <div id="updatePost">
+               
+               <div class="textComment">Comenta aquí</div>
+             
+              </div>
+              <div class="wall">
+              
+              <label class= "btnFile">
+               <input type="file" id="selectImage"  accept="image/jpeg, image/png, image/gif" class="upload">
+               <img src="img/foto.png" class="upImage"  style="width:30px; height:30px;">
+              </label> 
+              <img src="img/like.png"  id="like" style="width:30px; height:30px;">
+              <img src="img/comentario.png"  id="comment" style="width:30px; height:30px;">
+              </div>
+            </div>
+            <br>
+            <div class="templateComments">
+            <div class ="user-wall">
+             <img  class="user-foto" src="${dataUser.photoURL}"  style="width:50px; height:50px; border-radius:50%;  margin:auto; left:0; top:0; rigth:0; bottom:0;">
+            </div>
+           
+             <div id="updatePost">
+              
+              <div class="textComment">Comenta aquí</div>
+            
+             </div>
+             <div class="wall">
+             
+             <label class= "btnFile">
+              <input type="file" id="selectImage"  accept="image/jpeg, image/png, image/gif" class="upload">
+              <img src="img/foto.png" class="upImage"  style="width:30px; height:30px;">
+             </label> 
+             <img src="img/like.png"  id="like" style="width:30px; height:30px;">
+             <img src="img/comentario.png"  id="comment" style="width:30px; height:30px;">
+             </div>
+           </div>
   </aside>
     
           </div>

9412f2f369a53db4f2d4a9fda0ae81b7edca9055
jael-VRR <jrodriguezruidias@gmail.com>
2020-06-26 20:44:20 -0500

    almacenamiento

--- src/firebase-controller/storage-controller.js
+++ src/firebase-controller/storage-controller.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-shadow */
 
 //  referencia del storage
 export const storage = () => firebase.storage().ref();
 
 //  referencia de imagen del storage
-export const storageRef = (imgRef) => firebase.storage().ref().child(imgRef);
+export const storageRef = imgRef => firebase.storage().ref().child(imgRef);
 
 //  subir archivo
 export const put = (storage, file) => storage.put(file);
 
 //  obterner Url para descarga de archivo
-export const getDownloadURL = (storageRef) =>  storageRef.getDownloadURL();
+export const getDownloadURL = storageRef => storageRef.getDownloadURL();
